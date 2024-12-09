5f048398ceb286f032fa1208a0bd955adee4baff
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2024-12-09 13:26:16 -0500

    Se realizó el ejercicio 2

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-#Escriba un programa que muestre la tabla de multiplicar del
-#1 al 10 del número ingresado por el usuario:
-
+#Escriba un programa que genere todas las potencias de 2,
+#desde la 0-ésima hasta la ingresada por el usuario:
 
 number = int(input("Ingrese un número: "))
 
-i = 1
-while True:
-    
-    print(f"{number} x {i} = {number * i}")
-    i += 1
-    if i > 10:
-        break
+for i in range(number + 1):
+    potencia = 2 ** i
+
+    print(f"{potencia} ")
